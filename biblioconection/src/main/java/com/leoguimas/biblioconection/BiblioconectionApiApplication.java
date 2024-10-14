9ab3f3ceef0c45c215bc85c273bf9c04df473410
LeoGuimas <gleo.dev0@gmail.com>
2024-10-14 17:51:27 -0300

    starting docker

---
+++ biblioconection/src/main/java/com/leoguimas/biblioconection/BiblioconectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.leoguimas.biblioconection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioconectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioconectionApiApplication.class, args);
+	}
+
+}
---
+++ biblioconection/src/test/java/com/leoguimas/biblioconection/BiblioconectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoguimas.biblioconection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioconectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
